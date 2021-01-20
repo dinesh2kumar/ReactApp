018f3084dc914173581f5983019fd77b550166e4
@gupta.dinesh776 <gupta.dinesh776@gmail.com>
2021-01-20 18:59:26 +0530

    new commit

--- src/index.js
+++ src/index.js
@@ -1,38 +1,41 @@
-import React from 'react';
+import React from "react";
 import ReactDOM from 'react-dom';
-
+import SeasonDisplay from './SeasonDisplay'
+import Spinner from './Spinner'
+import './SeasonDisplay.css'
 
 
 class App extends React.Component{
-    constructor(props){
-        super(props);
-        this.state={lat: null};
+    state = {lat: null,errorMessage:''};
+    componentDidMount(){
         window.navigator.geolocation.getCurrentPosition(
-            (position)=> 
-            {
-                this.setState=({lat: position.coords.latitude});
-                console.log(position);
-            },
-            (err)=>console.log(err)  
+            position=> this.setState({lat: position.coords.latitude}),
+               err=>this.setState({errorMessage: err.message})    
         );
+    };
+
+renderContent(){
+          
+    if(this.state.errorMessage && !this.state.lat){
+        return <div className="error-message">Error:{this.state.errorMessage}</div>
     }
-    componentDidMount{
-        
+
+    if(!this.state.errorMessage && this.state.lat){
+        return <SeasonDisplay lat={this.state.lat} />
     }
+
+    return <Spinner message="Please accept location request"/>
+
+
+}
     render()
     {
-               
-            return(
-                 <div>Latitude: {this.state.lat} </div>
-                 )
-
+         return <div>{this.renderContent()}</div>
 
     }
 }
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <App/>,
+    document.querySelector("#root")
 );
