ad822055c0e3fc3bfa8d404ec713dc6ccdc9fec5
@gupta.dinesh776 <gupta.dinesh776@gmail.com>
2021-01-20 17:25:12 +0530

    Initial stage

--- src/index.js
+++ src/index.js
@@ -1,8 +1,34 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+
+
+class App extends React.Component{
+    constructor(props){
+        super(props);
+        this.state={lat: null};
+        window.navigator.geolocation.getCurrentPosition(
+            (position)=> 
+            {
+                this.setState=({lat: position.coords.latitude});
+                console.log(position);
+            },
+            (err)=>console.log(err)  
+        );
+    }
+    componentDidMount{
+        
+    }
+    render()
+    {
+               
+            return(
+                 <div>Latitude: {this.state.lat} </div>
+                 )
+
+
+    }
+}
 
 ReactDOM.render(
   <React.StrictMode>
@@ -10,8 +36,3 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
